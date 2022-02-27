ce346f3532ed5ba8de56d1dd37c93dbee36e8c78
Maryana Rulkevich <obrazcova.m@gmail.com>
2022-02-27 21:58:30 +0300

    done tomato

---
+++ tasks/medium/garden/gardener.py
@@ -0,0 +1,47 @@
+"""
+Садовник. Класс Gardener.
+
+Садовник, который может:
+1. Ухаживать за растением
+2. Собирать с него урожай
+
+Атрибуты:
+- **name** - имя садовника
+- **plants** - список с растениями, за которыми ухаживает садовник
+
+Методы:
+- инициализатор **\_\_init\_\_**, который принимает name - имя садовника
+  и args - произвольное количество кустов томата
+- метод **work()**, который заставляет садовника работать, что позволяет всем растениям расти
+- метод **harvest()**, который проверяет, все ли плоды созрели.
+  Если созрели все плоды - садовник собирает урожай (метод возвращает список всех томатов),
+  если нет - метод печатает предупреждение, что томаты не созрели и возвращает None.
+
+"""
+
+
+class Gardener:
+    name: str
+    plants: list
+
+    def __init__(self, name: str, *args):
+        self.name = name
+        self.plants = list(args)
+
+    def work(self):
+        for i in self.plants:
+            i.grow_all()
+
+    def harvest(self):
+        ready_plants = 0
+        for i in self.plants:
+            if i.all_are_ripe():
+                ready_plants += 1
+        if ready_plants == len(self.plants):
+            ready_tomato = []
+            for tomato_bush in self.plants:
+                ready_tomato += tomato_bush.give_away_all()
+                return ready_tomato
+            else:
+                print("Томаты не созрели")
+                return None
---
+++ tasks/medium/garden/main.py
@@ -0,0 +1,36 @@
+"""
+Задача.
+
+В блоке
+```python
+if __name__ == '__main__':
+    pass
+```
+выполнить следующие задания:
+1. Создать несколько объектов класса TomatoBush,
+   в каждом из которых будет минимум по 2 помидора
+2. Создайте объект Gardener, в который передать объекты, созданные в п.1
+3. Используя объект класса Gardener, поухаживайте за кустом с помидорами
+4. Попробуйте собрать урожай
+5. Если томаты еще не дозрели, продолжайте ухаживать за ними
+6. Соберите урожай
+7. Вывести сообщение о том, сколько томатов собрали
+"""
+from gardener import Gardener
+from tomato import Tomato
+from tomato_bush import TomatoBush
+
+
+if __name__ == '__main__':
+    tomato_1 = Tomato(1)
+    tomato_2 = Tomato(2)
+    tomato_3 = Tomato(3)
+    tomato_4 = Tomato(4)
+    bush_1 = TomatoBush(tomato_1, tomato_2)
+    bush_2 = TomatoBush(tomato_3, tomato_4)
+    gardener = Gardener('Marko', bush_1, bush_2)
+    gardener.work()
+    gardener.harvest()
+    gardener.work()
+    gardener.work()
+    print(f"Собрано {len(gardener.harvest())} томатов")
